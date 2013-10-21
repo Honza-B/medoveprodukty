a14d86f33ce875d67016dd666a97f14dc4cf69b3
Honza-B <benedikj.jan@gmail.com>
2013-10-21 21:31:56 +0200

    try orm

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php
-require_once 'src/db.handle.php';
+$dbconn = pg_connect("host=ec2-54-225-102-116.compute-1.amazonaws.com port=5432 dbname=dqc3ovvf3iq5n user=zpypggkdwxounx password=mNNTRvw5iCagVG9UapUgzJmRze sslmode=require options='--client_encoding=UTF8'") or die('Could not connect: ' . pg_last_error());
 
-$Nick = Conn::LoadAll();
-while ($data = pg_fetch_object($Nick)) {
-  echo $data->id;
-  echo $data->name;
+if(!$this->dbconn) 
+			echo 'neni spojeni';
+
+$nick = pg_query($dbconn, "SELECT * FROM nick ORDER BY id");
+
+while ($data = pg_fetch_object($nick)) {
+  echo $data->id . " je me id a";
+  echo $data->name . " je muj nick\n";
 }
 /*
 <style type="text/css">
