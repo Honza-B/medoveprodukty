73dbf8b42d8ea2d88526e27bf17e6fe6f92125bd
Honza-B <benedikj.jan@gmail.com>
2013-10-21 21:23:53 +0200

    try orm

--- src/db.handle.php
+++ src/db.handle.php
@@ -1,7 +1,7 @@
 <?php
 class Conn {
-	public $dbconn;
-	public $data;
+	public static $dbconn;
+	public static $data;
 	
 	public function __construct() {
 		$this->dbconn = pg_connect("host=ec2-54-225-102-116.compute-1.amazonaws.com port=5432 dbname=dqc3ovvf3iq5n user=zpypggkdwxounx password=mNNTRvw5iCagVG9UapUgzJmRze sslmode=require options='--client_encoding=UTF8'") or die('Could not connect: ' . pg_last_error());
