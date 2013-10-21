00da0c0afc6543e16cdfd5107707ae855551edc1
Honza-B <benedikj.jan@gmail.com>
2013-10-22 01:30:41 +0200

    make login action

--- admin/login.php
+++ admin/login.php
@@ -1,17 +1,20 @@
 <?php
 $dbconn = pg_connect("host=ec2-54-225-102-116.compute-1.amazonaws.com port=5432 dbname=dqc3ovvf3iq5n user=zpypggkdwxounx password=mNNTRvw5iCagVG9UapUgzJmRze sslmode=require options='--client_encoding=UTF8'") or die('Could not connect: ' . pg_last_error());
 
+$nick = $_POST["user"];
+$pass = sha1($_POST["pass"]);
+
 if(!$dbconn) {
 	echo 'Not connected to database';
 } else {
 	$user = pg_query($dbconn, "SELECT * FROM users");
 	$user = pg_fetch_object($user);
 	
-	$user->nick . '<br>';
-	$user->pass . '<br><br>';
+	echo $user->nick . '<br>';
+	echo $user->pass . '<br><br>';
 	
-	echo $_POST[user] . '<br>';
-	echo sha1($_POST[pass]);
+	echo $nick . '<br>';
+	echo $pass;
 	/*if($user->nick != $_POST["user"] || $user->pass != sha1($_POST["pass"])) {
 		echo 'spatny uzivatel nebo heslo';
 	} else {
