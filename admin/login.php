2a0a1595aa5f1117bad054d26a475f7bb3cf307a
Honza-B <benedikj.jan@gmail.com>
2013-10-22 00:54:21 +0200

    make login action

--- admin/login.php
+++ admin/login.php
@@ -1,2 +1,17 @@
 <?php
+$dbconn = pg_connect("host=ec2-54-225-102-116.compute-1.amazonaws.com port=5432 dbname=dqc3ovvf3iq5n user=zpypggkdwxounx password=mNNTRvw5iCagVG9UapUgzJmRze sslmode=require options='--client_encoding=UTF8'") or die('Could not connect: ' . pg_last_error());
+
+if(!$dbconn) {
+	echo 'Not connected to database';
+} else {
+	$user = pg_query($dbconn, "SELECT * FROM users");
+	$user = pg_fetch_object($user);
+
+	if($user->user != $_POST["user"] || $user->pass != sha1($_POST["pass"])) {
+		echo 'spatny uzivatel nebo heslo';
+	} else {
+		$_SESSION['User'] = serialize($user);
+		header("Location: http://medoveprodukty.herokuapp.com/admin/login.php");
+	}
+}
 ?>
