4ce804468f0d8254a977f0575222fc2ceb0a0d76
Honza-B <benedikj.jan@gmail.com>
2013-10-22 23:18:07 +0200

    make delte and edit action

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <meta http-equiv="content-type" content="text/html;charset=utf-8">
 <link rel="stylesheet" href="css/main.css">
+<?php 
+	$dbconn = pg_connect("host=ec2-54-225-102-116.compute-1.amazonaws.com port=5432 dbname=dqc3ovvf3iq5n user=zpypggkdwxounx password=mNNTRvw5iCagVG9UapUgzJmRze sslmode=require options='--client_encoding=UTF8'") or die('Could not connect: ' . pg_last_error());
+	$objProduct = pg_query($conn, "SELECT * FROM product");
+	
+?>
 <div id="wireframe">
     <div id="view">
         <div id="title">Lipový med</div>
@@ -11,7 +16,15 @@
         </div>
     </div>
     <div id="slider">
-        <div class="item"></div>
+    	<?php
+    	while($data = pg_fetch_object($objProduct)) {
+    		echo '
+    			<div class="item" id="'.$data->id.'">
+    				<img src="'.$data->path.'" title="'.$data->title.'" class="carousel">
+    			</div>
+    		';
+    	}
+        ?>
     </div>
     <div id="foo-link"><a href="admin/index.php">Login</a></div>
 </div>
