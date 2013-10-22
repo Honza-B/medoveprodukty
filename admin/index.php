a9b72574693cc2253f6518d61d7bdb4c5ffff1fc
Honza-B <benedikj.jan@gmail.com>
2013-10-22 18:01:17 +0200

    make login action

--- admin/index.php
+++ admin/index.php
@@ -2,6 +2,36 @@
 session_start();
 if(isset($_SESSION["User"])) {
 	echo $_SESSION["logMsg"];
+	
+	$conn = pg_connect("host=ec2-54-225-102-116.compute-1.amazonaws.com port=5432 dbname=dqc3ovvf3iq5n user=zpypggkdwxounx password=mNNTRvw5iCagVG9UapUgzJmRze sslmode=require options='--client_encoding=UTF8'") or die('Could not connect: ' . pg_last_error());
+	$objProduct = pg_query($conn, "SELECT * FROM product");
+	
+	echo '
+	<table>
+		<tr>
+			<td>ID</td>
+			<td>Název</td>
+			<td>Náhled</td>
+			<td>Popis</td>
+			<td></td>
+		</tr>
+	';
+
+	while($objProduct = pg_fetch_object($objProduct)) {
+		echo '
+			<tr>
+				<td'.$objProduct->id.'</td>
+				<td>'.$objProduct->title.'</td>
+				<td>'.$objProduct->path.'</td>
+				<td>'.$objProduct->description.'</td>
+				<td><button to-id="'.$objProduct->id.'" value="edit"><button to-id="'.$objProduct->id.'" value="delete"></td>
+			</tr>
+			';
+	}
+	
+	echo '</table>';
+	
+	
 } else {
 	echo $_SESSION["logMsg"];
 	echo '
