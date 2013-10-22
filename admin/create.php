2806875cf0f4b5a0f415676f1e30de8877592742
Honza-B <benedikj.jan@gmail.com>
2013-10-22 19:31:54 +0200

    make login action

--- admin/create.php
+++ admin/create.php
@@ -1,9 +1,12 @@
 <?php
 	echo '
 		<form action="create_edit.php" method="post">
+		Title<br>
 		<input type="text" name="title">
+		Path<br>
 		<input type="text" name="view">
-		<input type="textarea" name="desc">
+		Description<br>
+		<input type="textarea" name="desc"><br>
 		<input type="submit" value="Create">
 	</form>
 	';
