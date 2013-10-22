b8af370b7e4d5b7fee4f92a5855fc8813161098d
Honza-B <benedikj.jan@gmail.com>
2013-10-22 20:19:25 +0200

    make login action

--- admin/create_edit.php
+++ admin/create_edit.php
@@ -3,8 +3,12 @@
 	if(!isset($_SESSION["User"])) {
 		header('Location: index.php');
 	} else {
-		$conn = pg_connect("host=ec2-54-225-102-116.compute-1.amazonaws.com port=5432 dbname=dqc3ovvf3iq5n user=zpypggkdwxounx password=mNNTRvw5iCagVG9UapUgzJmRze sslmode=require options='--client_encoding=UTF8'") or die('Could not connect: ' . pg_last_error());
+		$dbconn = pg_connect("host=ec2-54-225-102-116.compute-1.amazonaws.com port=5432 dbname=dqc3ovvf3iq5n user=zpypggkdwxounx password=mNNTRvw5iCagVG9UapUgzJmRze sslmode=require options='--client_encoding=UTF8'") or die('Could not connect: ' . pg_last_error());
 		
+		if(!$dbconn) {
+			echo 'Not connected to database';
+		}
+			
 		$title = pg_escape_string($_POST["title"]);
 		$view = pg_escape_string($_POST["view"]);
 		$desc = pg_escape_string($_POST["desc"]);
