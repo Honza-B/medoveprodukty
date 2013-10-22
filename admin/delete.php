cfeb7151332fcde815f588ed49d61d946e531e30
Honza-B <benedikj.jan@gmail.com>
2013-10-22 22:23:29 +0200

    make insert action

---
+++ admin/delete.php
@@ -0,0 +1,26 @@
+<meta http-equiv="content-type" content="text/html;charset=utf-8">
+<?php
+	session_start();
+	if(!isset($_SESSION["User"])) {
+		header('Location: index.php');
+	} else {
+		$dbconn = pg_connect("host=ec2-54-225-102-116.compute-1.amazonaws.com port=5432 dbname=dqc3ovvf3iq5n user=zpypggkdwxounx password=mNNTRvw5iCagVG9UapUgzJmRze sslmode=require options='--client_encoding=UTF8'") or die('Could not connect: ' . pg_last_error());
+		
+		if(!$dbconn) {
+			echo 'Not connected to database';
+		}
+			
+		$id = $_GET["id"];
+		
+		$delte = pg_query($dbconn,"DELETE FROM product WHERE id='$id'");
+		if(!$delete) {
+			echo pg_last_error();
+			echo 'okej';
+		} else {
+		}
+		
+		
+		
+		//header('Location: index.php');
+	}
+?>
