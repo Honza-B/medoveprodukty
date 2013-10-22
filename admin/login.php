0da26c8df854264c3a05bf065e9db63326e4654a
Honza-B <benedikj.jan@gmail.com>
2013-10-22 10:31:18 +0200

    make login action

--- admin/login.php
+++ admin/login.php
@@ -1,13 +1,15 @@
 <?php
+session_start();
+
 $dbconn = pg_connect("host=ec2-54-225-102-116.compute-1.amazonaws.com port=5432 dbname=dqc3ovvf3iq5n user=zpypggkdwxounx password=mNNTRvw5iCagVG9UapUgzJmRze sslmode=require options='--client_encoding=UTF8'") or die('Could not connect: ' . pg_last_error());
 
-$nick = $_POST["user"];
+$nick = pg_escape_string(strip_html($_POST["user"]));
 $pass = sha1($_POST["pass"]);
 
 if(!$dbconn) {
 	echo 'Not connected to database';
 } else {
-	$user = pg_query($dbconn, "SELECT * FROM users");
+	$user = pg_query($dbconn, "SELECT * FROM users WHERE nick='$nick' and pass='$pass'");
 	$user = pg_fetch_object($user);
 
 	if($user->nick != $nick || $user->pass != $pass) {
